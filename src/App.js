183a2d07dd65745986b78fde1dbb2c4c783b14e5
codeflar <lawsonluke9@gmail.com>
2023-04-04 20:04:54 +0100

    created login page

--- src/App.js
+++ src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+ import React, { PureComponent } from 'react';
+ import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
+ import Login from './pages/Login';
+ 
+ class App extends PureComponent {
+   render(){
+     return(
+       <Router>
+       <div>
+       <Switch>
+       <Route path="/" exact component={Login} />
+       </Switch>
+       </div>
+     </Router>
+     )
+   }
+ }
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+ export default App;
---
+++ src/pages/Login.js
@@ -0,0 +1,13 @@
+import React, { PureComponent } from 'react';
+
+class Login extends PureComponent {
+  render(){
+    return(
+      <div>
+      <p>Hello</p>
+      </div>
+    )
+  }
+}
+
+export default Login
